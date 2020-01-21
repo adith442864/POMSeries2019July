d6a3f987386b2ef217b3c1883b3c76144a2377f4
adith442864 <adith442864@gmail.com>
2020-01-21 21:24:09 +0530

    modified changes to pom.xml, base page, listeners added

--- src/main/java/com/qa/hubspot/base/BasePage.java
+++ src/main/java/com/qa/hubspot/base/BasePage.java
@@ -1,10 +1,14 @@
 package com.qa.hubspot.base;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -17,6 +21,13 @@ public class BasePage {
 	
 	public WebDriver driver;
 	public Properties prop;
+	
+public static ThreadLocal<WebDriver> tldriver = new ThreadLocal<WebDriver>();
+	
+	
+	public static synchronized WebDriver getDriver() {
+		return tldriver.get();
+	}
 	
 	public Properties initialize_properties() {
 		
@@ -83,6 +94,18 @@ public class BasePage {
 		catch(Exception e) {
 			System.out.println("Exception occured while closing the browser");
 		}
+	}
+	
+	public String getScreenshot() {
+		File src = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
+		File destination = new File(path);
+		try {
+			FileUtils.copyFile(src, destination);
+		} catch (IOException e) {
+			System.out.println("Capture Failed " + e.getMessage());
+		}
+		return path;
 	}
 
 		
